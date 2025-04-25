d52a67d986ce32d2fe1e627cc503df16f4d46f2e
mu464 <naume4murungi@gmail.com>
2025-04-26 00:29:45 +0200

    Group assignment on php

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Murungi Naomey PHP</title>
+</head>
+<body>
+    <h1>Murungi Naomey PHP</h1>
+
+    <pre>
+    M   M
+    MM MM
+    M M M
+    M   M
+    M   M
+    M   M
+    M   M
+    </pre>
+
+    <p>SHA256 of "Murungi Naomey":</p>
+    <p>
+    <?php
+        echo hash('sha256', 'Murungi Naomey');
+    ?>
+    </p>
+</body>
+</html>
